2b3420c5db9b9b71492a1eca0c592157d8fabb60
littwa <littwa777@gmail.com>
2020-08-15 22:28:17 +0300

    asyncComponent

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import routes from "../services/routes";
 import Home from "../views/Home";
-import Details from "../views/Details";
-import Movies from "../views/Movies";
 import Layout from "./Layout";
 import PageNotFound from "../views/PageNotFound";
-import routes from "../services/routes";
+// import Movies from "../views/Movies";
+// import Details from "../views/Details";
+import asyncComponent from "./hoc/asyncComponent";
+const AsyncComponentDetails = asyncComponent(() => import("../views/Details"));
+// const AsyncComponentMovies = asyncComponent(() => import("../views/Movies"));
 
 class App extends React.Component {
  render() {
@@ -13,8 +16,10 @@ class App extends React.Component {
    <Layout>
     <Switch>
      <Route path={routes.home} exact component={Home} />
-     <Route path={routes.movies} exact component={Movies} />
-     <Route path={routes.details} component={Details} />
+     <Route path={routes.movies} exact component={asyncComponent(() => import("../views/Movies"))} />
+     <Route path={routes.details} component={AsyncComponentDetails} />
+     {/* <Route path={routes.movies} exact component={Movies} /> */}
+     {/* <Route path={routes.details} component={Details} /> */}
      <Route component={PageNotFound} />
     </Switch>
    </Layout>
